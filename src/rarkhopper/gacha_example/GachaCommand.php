e48fb72729e4cda7737d086fd35737af28afcdce
Rark7040 <crimera.hello@gmail.com>
2022-07-15 11:33:10 +0900

    exec

--- src/rarkhopper/gacha_example/GachaCommand.php
+++ src/rarkhopper/gacha_example/GachaCommand.php
@@ -4,7 +4,26 @@ declare(strict_types=1);
 namespace rarkhopper\gacha_example;
 
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use rarkhopper\gacha\Gacha;
+
 
 class GachaCommand extends Command{
+	protected Gacha $gacha;
 
+	public function __construct(){
+		parent::__construct('gacha_example');
+		$this->setPermission('gacha_example.command.public');
+		$this->gacha = $this->createGacha();
+	}
+
+	protected function createGacha():Gacha{
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+		if(!$this->testPermission($sender)) return;
+		if(!$sender instanceof Player) return;
+
+	}
 }
