717dd5a6bd1e7057e46cc020c950e386ae0227d3
Rark7040 <crimera.hello@gmail.com>
2022-07-15 11:36:51 +0900

    has, consume

--- src/rarkhopper/gacha_example/ExampleTicket.php
+++ src/rarkhopper/gacha_example/ExampleTicket.php
@@ -3,8 +3,16 @@ declare(strict_types=1);
 
 namespace rarkhopper\gacha_example;
 
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
 use rarkhopper\gacha\ITicket;
 
 class ExampleTicket implements ITicket{
+	public function has(Player $player, int $count):bool{
+		return $player->getInventory()->contains(VanillaItems::GOLD_NUGGET()->setCount($count));
+	}
 
+	public function consume(Player $player, int $count):void{
+		$player->getInventory()->remove(VanillaItems::GOLD_NUGGET()->setCount($count));
+	}
 }
